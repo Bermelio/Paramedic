6f05598c73806cdcd9dd0278f23fa9a689d4bae3
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2025-07-16 23:06:01 -0300

    creando la carpeta controllers, config, middleware

---
+++ Backend/src/middleware/auth.route.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.post('/api/login', async (req: Request, res: Response) => {
+
+});
+
+export default router;
